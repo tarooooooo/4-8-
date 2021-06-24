da7c703f5b762c1943713296bbc9954e5c1cb25a
EC2 Default User <ryutaromaeda83@gmail.com>
2021-06-24 14:07:20 +0000

    book.newでデータ取得

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,18 @@
 class BooksController < ApplicationController
   def top
-    @book=Book.new
+   
   end
   def create
-    book=Book.new(book_params)
+    @book=Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
   def index
+     @book=Book.new
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
@@ -16,6 +20,6 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.require(:book).permit(:title,:body)
+    params.permit(:title,:body)
   end
 end
